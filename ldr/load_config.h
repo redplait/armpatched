8db044f49fdf952aea8213787902772e0db3ef2f
redplait <>
2020-04-09 18:19:13 +0300

    add missed file

---
+++ ldr/load_config.h
@@ -0,0 +1,51 @@
+#pragma once
+
+typedef struct _rfg_IMAGE_LOAD_CONFIG_CODE_INTEGRITY {
+    WORD    Flags;          // Flags to indicate if CI information is available, etc.
+    WORD    Catalog;        // 0xFFFF means not available
+    DWORD   CatalogOffset;
+    DWORD   Reserved;       // Additional bitmask to be defined later
+} rfg_IMAGE_LOAD_CONFIG_CODE_INTEGRITY, *Prfg_IMAGE_LOAD_CONFIG_CODE_INTEGRITY;
+
+typedef struct _rfg_IMAGE_LOAD_CONFIG_DIRECTORY64 {
+    DWORD      Size;
+    DWORD      TimeDateStamp;
+    WORD       MajorVersion;
+    WORD       MinorVersion;
+    DWORD      GlobalFlagsClear;
+    DWORD      GlobalFlagsSet;
+    DWORD      CriticalSectionDefaultTimeout;
+    ULONGLONG  DeCommitFreeBlockThreshold;
+    ULONGLONG  DeCommitTotalFreeThreshold;
+    ULONGLONG  LockPrefixTable;                // VA
+    ULONGLONG  MaximumAllocationSize;
+    ULONGLONG  VirtualMemoryThreshold;
+    ULONGLONG  ProcessAffinityMask;
+    DWORD      ProcessHeapFlags;
+    WORD       CSDVersion;
+    WORD       DependentLoadFlags;
+    ULONGLONG  EditList;                       // VA
+    ULONGLONG  SecurityCookie;                 // VA
+    ULONGLONG  SEHandlerTable;                 // VA
+    ULONGLONG  SEHandlerCount;
+    ULONGLONG  GuardCFCheckFunctionPointer;    // VA
+    ULONGLONG  GuardCFDispatchFunctionPointer; // VA
+    ULONGLONG  GuardCFFunctionTable;           // VA
+    ULONGLONG  GuardCFFunctionCount;
+    DWORD      GuardFlags;
+    rfg_IMAGE_LOAD_CONFIG_CODE_INTEGRITY CodeIntegrity;
+    ULONGLONG  GuardAddressTakenIatEntryTable; // VA
+    ULONGLONG  GuardAddressTakenIatEntryCount;
+    ULONGLONG  GuardLongJumpTargetTable;       // VA
+    ULONGLONG  GuardLongJumpTargetCount;
+    ULONGLONG  DynamicValueRelocTable;         // VA
+    ULONGLONG  CHPEMetadataPointer;            // VA
+    ULONGLONG  GuardRFFailureRoutine;          // VA
+    ULONGLONG  GuardRFFailureRoutineFunctionPointer; // VA
+    DWORD      DynamicValueRelocTableOffset;
+    WORD       DynamicValueRelocTableSection;
+    WORD       Reserved2;
+    // since w10 build 15003 ?
+    ULONGLONG  GuardRFVerifyStackPointerFunctionPointer; // VA
+    DWORD      HotPatchTableOffset;
+} rfg_IMAGE_LOAD_CONFIG_DIRECTORY64, *Prfg_IMAGE_LOAD_CONFIG_DIRECTORY64;
