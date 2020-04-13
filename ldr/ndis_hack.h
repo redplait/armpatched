2f999434d04d1f3a1c211a69b95443276813e66a
redplait <>
2020-04-13 23:32:22 +0300

    iat fix in ndis_hack

--- ldr/ndis_hack.h
+++ ldr/ndis_hack.h
@@ -22,7 +22,7 @@ class ndis_hack: public drv_hack
 {
   public:
     ndis_hack(arm64_pe_file *pe, exports_dict *ed, module_import *iat)
-     : drv_hack(pe, ed, m_iat)
+     : drv_hack(pe, ed, iat)
     {
       zero_data();
     }
@@ -34,7 +34,6 @@ class ndis_hack: public drv_hack
     void zero_data();
     void collect_calls(PBYTE psp, std::set<PBYTE> &, const char *s_name);
     int hack_lock_list(PBYTE psp, DWORD num, PBYTE &lock, PBYTE &list);
-    module_import *m_iat;
     // output data
     PBYTE m_ndisProtocolListLock;
     PBYTE m_ndisProtocolList;
