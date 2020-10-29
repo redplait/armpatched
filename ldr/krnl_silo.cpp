a359d14910d5d11c1e634b148533bc77e22b80e1
redplait <>
2020-10-29 17:48:11 +0300

    rewrite hack_start_silo with traverse_simple_state_graph

--- ldr/krnl_silo.cpp
+++ ldr/krnl_silo.cpp
@@ -160,67 +160,36 @@ int ntoskrnl_hack::hack_silo_global(PBYTE psp)
 
 int ntoskrnl_hack::hack_start_silo(PBYTE psp)
 {
-  statefull_graph<PBYTE, int> cgraph;
-  std::list<std::pair<PBYTE, int> > addr_list;
-  auto curr = std::make_pair(psp, 0);
-  addr_list.push_back(curr);
-  int edge_gen = 0;
-  int edge_n = 0;
-  while( edge_gen < 100 )
-  {
-    for ( auto iter = addr_list.cbegin(); iter != addr_list.cend(); ++iter )
-    {
-      psp = iter->first;
-      int state = iter->second;
-      if ( m_verbose )
-        printf("hack_start_silo: %p, state %d, edge_gen %d, edge_n %d\n", psp, state, edge_gen, edge_n);
-      if ( cgraph.in_ranges(psp) )
-        continue;
-      if ( !setup(psp) )
-        continue;
-      regs_pad used_regs;
-      for ( ; ; )
+  traverse_simple_state_graph(psp, [&](int *state, regs_pad *used_regs) -> int
+   {
+      if ( is_add() )
       {
-        if ( !disasm(state) || is_ret() )
-          break;
-        if ( check_jmps(cgraph, state) )
-          continue;
-        if ( is_adrp(used_regs) )
-          continue;
-        if ( is_add() )
+        PBYTE what = (PBYTE)used_regs->add(get_reg(0), get_reg(1), m_dis.operands[2].op_imm.bits);
+        if ( !in_section(what, ".data") )
         {
-          PBYTE what = (PBYTE)used_regs.add(get_reg(0), get_reg(1), m_dis.operands[2].op_imm.bits);
-          if ( !in_section(what, ".data") )
-          {
-             used_regs.zero(get_reg(0));
-             continue;
-          }
-          if ( state )
-          {
-            if ( what != aux_PsInitialSystemProcess )
-            {
-              m_PspSiloMonitorList = what;
-              goto end;
-            }
-          }
+           used_regs->zero(get_reg(0));
+           return 0;
         }
-        PBYTE addr = NULL;
-        if ( is_bl_jimm(addr) )
+        if ( *state )
         {
-          if ( !state && (addr == aux_ExAcquirePushLockExclusiveEx) )
+          if ( what != aux_PsInitialSystemProcess )
           {
-             m_PspSiloMonitorLock = (PBYTE)used_regs.get(AD_REG_X0);
-             state = 1;
+             m_PspSiloMonitorList = what;
+             return 1;
           }
         }
       }
-      cgraph.add_range(psp, m_psp - psp);
-    }
-    // prepare for next edge generation
-    edge_gen++;
-    if ( !cgraph.delete_ranges(&cgraph.ranges, &addr_list) )
-      break;    
-  }
-end:
+      PBYTE addr = NULL;
+      if ( is_bl_jimm(addr) )
+      {
+        if ( !*state && (addr == aux_ExAcquirePushLockExclusiveEx) )
+        {
+           m_PspSiloMonitorLock = (PBYTE)used_regs->get(AD_REG_X0);
+           *state = 1;
+        }
+      }
+      return 0;
+   }, "hack_start_silo");
+
   return (m_PspSiloMonitorLock != NULL) && (m_PspSiloMonitorList != NULL);
 }
